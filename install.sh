5c7228d20fdb67c2ebdb5bbc03113d1c04d51779
warning31 <mustafaavci33@gmail.com>
2023-10-21 12:59:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,22 +58,26 @@ _klasorolustur() {
 
 _repoekleniyor() {
     echo -n "Repo Ekleniyor" 1>&3
-   apt-get update
-   sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# Set up stable repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
 
+    # "yes" seçeneğini otomatik olarak kabul et
+    user_input="yes"
 
-    echo "Repo Eklendi" 1>&3
+    if [ "$user_input" = "yes" ]; then
+        # Docker yüklemesini gerçekleştir
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "Repo Eklendi" 1>&3
+    else
+        echo "Docker yüklemesi iptal edildi."
+    fi
 }
+
 
 _dockerkuruluyor() { 
     echo -n "Docker Kuruluyor" 1>&3
