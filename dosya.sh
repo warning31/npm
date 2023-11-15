eb8b953b8047b21d1e32b1d0ed7e2238c85b6903
warning31 <mustafaavci33@gmail.com>
2023-11-15 19:47:13 +0300

    Update dosya.sh

--- dosya.sh
+++ dosya.sh
@@ -4,7 +4,7 @@
 source_host="88.99.146.8"
 source_user="root"
 source_path_prefix="/home/hls/public_html/cdn/down/"  # Değişken kısım
-source_password="DxwwEwuGhryT9d"   # Belirtilmiş şifre
+source_password="DxwwEwuGhryT9d"
 
 # Kullanıcıdan alınan kaynak yolunu kullanarak tam kaynak yolunu oluştur
 source_path="$source_path_prefix$1"
@@ -13,7 +13,7 @@ source_path="$source_path_prefix$1"
 target_host="49.12.8.157"
 target_user="root"
 target_path="/home/hls/public_html/cdn/down/"  # Sabit kısım
-target_password="Ma.437588"  # Belirtilmiş şifre
+target_password="Ma.437588"
 
 # Kontrol: Kaynak yolu belirtilmiş mi?
 if [ -z "$1" ]; then
@@ -25,8 +25,8 @@ fi
 # Dosya kopyalama
 copy_files() {
     echo "Dosyalar kopyalanıyor..."
-    # -B parametresi kullanıcıdan şifreyi alır
-    sshpass -p "$source_password" scp -r "$source_user@$source_host:$source_path" "$target_user@$target_host:$target_path"
+    # SCP komutu ile dosyaları kopyala
+    scppass="$source_password" scp -r "$source_user@$source_host:$source_path" "$target_user@$target_host:$target_path"
     echo "Dosya kopyalama işlemi tamamlandı."
 }
 
